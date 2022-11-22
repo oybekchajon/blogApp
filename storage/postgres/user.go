e54ebd48135f875647db23b9725263c70e527a56
[oybek baxodir] <[oybekbakhodirov4@gmail.com]>
2022-11-22 13:02:15 +0500

    thrid commit

--- storage/postgres/user.go
+++ storage/postgres/user.go
@@ -1,1 +1,11 @@
 package postgres
+
+
+
+// echo "# blogApp" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin git@github.com:oybekchajon/blogApp.git
+// git push -u origin main
